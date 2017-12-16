4ab5081a2734d891c1a6a9e9724ccd67a267d55e
Wojciech Wójcik <wojtek.wroclaw@hotmail.com>
2017-12-16 19:55:55 +0100

    working buttons, thread, stoper

---
+++ oldKolokwium201617/src/oldKolokwium201617/KolokwiumApp.java
@@ -0,0 +1,100 @@
+package oldKolokwium201617;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class KolokwiumApp extends JFrame implements ActionListener,MouseListener {
+
+	public static void main(String[] args) {
+		new KolokwiumApp();
+	}
+	
+	JPanel panelik = new JPanel();
+	
+	JButton author = new JButton("autor");
+	JButton restartStopera = new JButton("Restart");
+	
+	JLabel stoper = new JLabel("0");
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public KolokwiumApp() {
+		StoperThread stoperThread = new StoperThread(stoper);
+		setTitle("Wojciech Wójcikik");
+		setSize(400, 400);
+		setResizable(false);
+		
+
+				
+		author.addActionListener(this);
+		restartStopera.addActionListener(this);
+		
+		panelik.add(stoper);
+		panelik.add(restartStopera);
+		panelik.add(author);
+		
+		setContentPane(panelik);
+		setVisible(true);
+		stoperThread.run();
+	}
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object src =  e.getSource();
+		
+		if (src == author) {
+			JOptionPane.showMessageDialog(this, "Wojciech Wójcik \n 23XXXX");
+		}
+		if (src == restartStopera) {
+			stoper.setText("0");
+		}
+		
+	}
+
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
